2dfb4f50aed266926c6662fba5179b3c484e92f8
tangpnm <tang_6528@hotmail.com>
2020-11-02 21:52:47 +0700

    deploy-test

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import json
+import numpy as np
+import traceback
+from detail_model import test, preprocess, get_prediction
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "This is the emotion classification"
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+
+    data = test()
+    if data:
+        try:
+            # 1 load message
+            content = request.get_json()
+            msg = content['sentance']
+            print(msg)
+
+            # 2 proprocess the data
+            prep = preprocess(msg)
+
+            # 3 prediction
+            prediction = get_prediction(prep)
+            # print("here:", prediction)
+
+            # 4 return json data
+            return jsonify({'result': prediction})
+
+        except:
+            return jsonify({'result': traceback.format_exc()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ detail_model.py
@@ -0,0 +1,35 @@
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+#load model
+path_model = './model/LSTM_w2v.h5'
+
+# Loading the model and assign it to the predictor
+loaded_model = load_model(path_model)
+
+#Initialize the value
+tokenizer = Tokenizer()
+max_seq_len = 500
+class_names = ['joy', 'fear', 'anger', 'sadness', 'neutral']
+
+def test():
+    # testModel = loaded_model.get_classes()
+
+    return "testModel"
+
+def preprocess(message):
+
+    seq = tokenizer.texts_to_sequences(message)
+    padded = pad_sequences(seq, maxlen=max_seq_len)
+
+    return padded
+
+
+def get_prediction(padded):
+
+    predict = loaded_model.predict(padded)
+    answer = class_names[np.argmax(predict)]
+
+    return answer
